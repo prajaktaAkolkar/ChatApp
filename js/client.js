b82d7c4e3f62198e81a5914f92e723b916f8c564
prajaktaAkolkar <prajakta.akolkar@mangoitsolutions.in>
2021-11-20 18:03:26 +0530

    update

--- js/client.js
+++ js/client.js
@@ -1,9 +1,15 @@
-const socket = io('http://localhost:8000');
+const socket = io('http://localhost:7000');
 
+
+//Get DOM elements in respective Js variables
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
 const messageContainer = document.querySelector(".container");
 
+
+
+
+//Functions which will appendevent Info to the Container
 const append = (message, position)=>{
   const messageElement = document.createElement('div');
   messageElement.innerText = message;
@@ -12,27 +18,41 @@ const append = (message, position)=>{
   messageContainer.append(messageElement);
 }
 
-form.addEventListener('submit',(e)=>{
-  e.preventDefault();
-  const message = messageInput.value;
-  append(`You:${message}`,'right');
-  socket.emit('send',message);
-  messageInput.value ='';
-})
 
-const name = prompt("Enter your name toJoin");
-socket.emit('new-user-joined', name) ;
+//Ask new user for his/her name and let the server know
+const name = prompt("Enter your name to Join");
+socket.emit('new-user-joined', name );
 
-socket.on('user-joined',name =>{
+
+//If new user Joins,recive his/her name from the server 
+socket.on('user-joined',name => {
  append(`${name} joined the chat`,'right');
 })
 
 
+
+
+//If server Sends the message receive it
 socket.on('receive',data => {
     append(`${data.name}:${data.message}`,'left');
+    
 })
 
-
+//If user Leave the chat append the info to the container
 socket.on('left', name =>{
     append(`${name} left the chat`, 'left');
 })
+
+//if the form get submitted, send server the mesage
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`You:${message}`,'right');
+    socket.emit('send',message);
+    messageInput.value ='';
+   
+  })
+  
+  
+ 
+   
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,22 +1,91 @@
+
+    
 //node server 
-const io = require('socket.io')(8000);
+const io = require('socket.io')(7000);
+const mysql = require("mysql2");
 const users = {};
+var http = require("http").createServer(app);
+var express = require("express");
+
+const { connect } = require('http2');
+
+var app = express();
+app.use(function (request, result, next) {
+	result.setHeader("Access-Control-Allow-Origin", "*");
+	next();
+});
+
+
+//Create Connection 
+var conn = mysql.createConnection({
+   host :"localhost",
+   user : "root",
+   password : "",
+   database :"chat_app"
+});
+
+
+
+conn.connect(function(error){
+    //shoe err ifany
+    if(error){
+        console.log("not connected");
+    }
+    else{
+        console.log("Connected to Mysql")
+    }
+})
+
+
 
 io.on('connection',(socket)=>{
+//If any user Joins,let other users connected to the server know
+
     socket.on('new-user-joined', (name) =>{
         console.log("new User",name);
     users[socket.id] = name;
     socket.broadcast.emit('user-joined',name);
+   
     })
 
+    
+   
+
+    //If some one sends the message broadcast it to other people
     socket.on('send',message =>{
-        socket.broadcast.emit('receive', {message : message , name: users[socket.id]})
+       
+        socket.broadcast.emit('receive', {message : message , name: users[socket.id]});
+
+
+        
+        /*
+        //SAve message in database
+        conn.query("INSERT INTO messages (message) VALUES ('"+ message +"')", function (error,result){
+
+        });*/
+        conn.query("INSERT INTO messages (message,name,send_name) VALUES ('" + message + "','" +users[socket.id]+"','"+socket.id+"')", function (error, result) {
+			console.log("data inserted");
+		});
+  
+      
     });
 
+
+    //If someone leavethe chatlet others know
     socket.on('disconnect', message => {
         socket.broadcast.emit('left' , users[socket.id]);
         delete users[socket.id];
     });
 })
 
-console.log("server running at 8000")
+
+//Create API for get message
+app.get("/get_messages",(request,result)=>{
+   conn.query("SELECT * FROM messages",function(error,messages){
+       result.end(JSON.stringify(message));
+   })
+})
+
+
+console.log("server running at 7000");
+
